4c60f7a7b4cb40739751991d525082264404ae39
yuriy_demin <y.y.demin@gmail.com>
2021-01-21 23:12:49 +0500

    add routes; add view; add dependecies

---
+++ lib/views/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  static const routeName = '/';
+  HomeView({Key key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
