f2bc00583c5bc59d51935c505ec27bc9cb66499e
yuriy_demin <y.y.demin@gmail.com>
2021-01-22 09:27:58 +0500

    add water amount to profile model

--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:water_reminder/models/common_model.dart';
+import 'package:water_reminder/models/math_model.dart';
 
 class Profile {
   final Gender gender;
   final int weight;
   final Activity activity;
-  //final int requiredWaterAmount;
+  final int requiredWaterAmount;
 
   Profile({
     @required this.gender,
     @required this.weight,
     @required this.activity,
-  });
+  }) : requiredWaterAmount =
+            Math.getRequiredWaterAmount(gender, weight, activity);
 }
