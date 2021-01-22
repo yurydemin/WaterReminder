e593a37291059ddb1f50106cd3cc58e335c8219c
yuriy_demin <y.y.demin@gmail.com>
2021-01-22 09:19:06 +0500

    add water calculations

--- lib/models/math_model.dart
+++ lib/models/math_model.dart
@@ -1,16 +1,39 @@
 import 'package:water_reminder/models/common_model.dart';
 
 class Math {
-  int _getWeightCoef(Gender gender) {}
+  static int _getWeightCoef(Gender gender) {
+    return (gender == Gender.male ? 35 : 31);
+  }
 
-  int _getActivityCoef(
+  static int _getAdditionalWaterAmount(
     Gender gender,
     Activity activity,
-  ) {}
+  ) {
+    int genderCoef = (gender == Gender.male ? 600 : 400);
+    double activityCoef;
+    switch (activity) {
+      case Activity.low:
+        activityCoef = 0;
+        break;
+      case Activity.medium:
+        activityCoef = 0.5;
+        break;
+      case Activity.high:
+        activityCoef = 2.0;
+        break;
+      default:
+        activityCoef = 0;
+        break;
+    }
+    return (genderCoef * activityCoef).toInt();
+  }
 
   static int getRequiredWaterAmount(
     Gender gender,
     int weight,
     Activity activity,
-  ) {}
+  ) {
+    return weight * _getWeightCoef(gender) +
+        _getAdditionalWaterAmount(gender, activity);
+  }
 }
