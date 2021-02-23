d94395b2931cae1e54fe701821fb0d0197c4f397
yuriy_demin <y.y.demin@gmail.com>
2021-02-23 18:41:17 +0500

    Add localization support; prepare empty locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_i18n/loaders/file_translation_loader.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:water_reminder/helpers/notifications_helper.dart';
 import 'package:water_reminder/providers/drink_water_provider.dart';
 import 'package:water_reminder/views/home_view.dart';
 
 void main() async {
+  final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
+    translationLoader: FileTranslationLoader(
+      useCountryCode: false,
+      fallbackFile: 'en',
+      basePath: 'assets/locales',
+      forcedLocale: Locale('en'),
+    ),
+  );
   WidgetsFlutterBinding.ensureInitialized();
+  await flutterI18nDelegate.load(null);
   await NotificationsHelper.init();
-  runApp(MyApp());
+  runApp(MyApp(flutterI18nDelegate));
 }
 
 class MyApp extends StatelessWidget {
+  final FlutterI18nDelegate flutterI18nDelegate;
+
+  MyApp(this.flutterI18nDelegate);
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -30,6 +47,12 @@ class MyApp extends StatelessWidget {
         routes: {
           '/': (BuildContext context) => HomeView(),
         },
+        localizationsDelegates: [
+          flutterI18nDelegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
       ),
     );
   }
